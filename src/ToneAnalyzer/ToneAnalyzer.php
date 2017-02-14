8e52471b5f95856bec47e073eb5799227ccf3f41
Michael <mihui.net@outlook.com>
2017-02-14 10:17:34 +0800

    Update ToneAnalyzer.php
    Changed namespace to WatsonSDK

--- src/ToneAnalyzer/ToneAnalyzer.php
+++ src/ToneAnalyzer/ToneAnalyzer.php
@@ -5,7 +5,7 @@
  * Date: 09/02/2017
  * Time: 9:53 AM
  */
-namespace Watson\Service;
+namespace WatsonSDK\Service;
 require_once 'vendor/autoload.php';
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7;
