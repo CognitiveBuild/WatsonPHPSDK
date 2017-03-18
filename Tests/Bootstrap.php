dac262a549cae2e61ab453e670d9b34d7388573b
Michael <mihui@cn.ibm.com>
2017-03-19 01:48:37 +0800

    Added a base class of unit test

--- Tests/Bootstrap.php
+++ Tests/Bootstrap.php
@@ -1,4 +1,5 @@
 <?php
 require __DIR__ . '/Environment.php';
+require __DIR__ . '/BaseTestCase.php';
 $loader = require __DIR__ . '/../vendor/autoload.php';
 $loader->add('WatsonSDK\\Tests\\', __DIR__);
